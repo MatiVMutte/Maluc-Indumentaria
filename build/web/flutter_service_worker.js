76ab24e4de77f8c648c7bf57beec3f67cd321fb8
MatiVMutte <mativmutte@gmail.com>
2024-06-24 10:28:58 -0300

    Segundo commit

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -25,7 +25,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "6674ea5d5f1318586b579128a284cd23
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "6db99a0fcb2da3c99187e0c3bfbb20ff",
+"flutter_bootstrap.js": "75fdd539f9e147a0d6cfada1caeacbcd",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
